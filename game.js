87ea9beb001b812f0e32334bffbe57bfc68ebc5e
Matt <mparman6@yahoo.com>
2015-11-16 18:21:00 -0500

    updates to js

--- game.js
+++ game.js
@@ -1,11 +1,31 @@
-document.getElementById("start-button").addEventListener("click", countDown);
-var count;
+var timer;
+var remainingTime = 20;
+
+function beginCount() {
+  remainingTime = 20;
+  timer = setInterval(gameTimer, 1000);
+}
    
+function gameTimer() {
+  if(remainingTime > 0) {
+    document.getElementById("start-button").innerHTML = remainingTime -=1;
+  }
+  else {
+    document.getElementById("start-button").innerHTML = "START"
+    clearInterval(gameTimer);
+  }
+}
+
+document.getElementById("start-button").addEventListener("click", beginCount);
+
+function picEvent() {
+  for(var i = 0; i < document.getElementByClassName("img-thumbnail").length; i++)
+    document.getElementByClassName("img-thumbnail")[i].addEventListener("click", counter);
+}
+
+function counter() {
+  count++;
+}
 
 
 
-var clicks = 0;
-    function onClick() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-};
